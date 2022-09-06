cd6e5bf6b6d1f45d8b0ee075b5ede59a6f919512
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-09-06 09:23:59 -0500

    feat: added navlinks to welcome and products page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<MainHeader />
+			<main>
+				<Route path="/welcome">
+					<Welcome />
+				</Route>
+				<Route path="/products">
+					<Products />
+				</Route>
+			</main>
+		</div>
+	);
 }
 
 export default App;
